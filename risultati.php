63ef39392ec7fdc7ebaef4243d30453fdfab3ffe
Gabriel <gabrielsiano2006@gmail.com>
2024-10-22 10:44:39 +0300

    Commit 1

---
+++ risultati.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $nome = $_GET["nome"];
+    $cognome = $_GET["cognome"];
+    $data = $_GET["data"];
+    $orario = $_GET["orario"];
+    $mezzo = $_GET["mezzo"];
+
+
+
+
+echo"
+
+    <table>
+    
+    <tr>
+    <th>Nome</th>
+    <th>Cognome</th>
+    <th>Data di Nascita</th>
+    <th>Orario di Arrivo a Casa</th>
+    <th>Mezzo di Trasporto</th>
+    <th>Corsi</th>
+    </tr>
+
+
+    
+
+
+
+
+
+
+
+
+    </table>
+
+
+    "
+    
+
+
+?>
+</body>
+</html>
