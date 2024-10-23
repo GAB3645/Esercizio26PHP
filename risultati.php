371c3cc18fa2d1e3f6fc799d1c2c793484fd832b
Gabriel <gabrielsiano2006@gmail.com>
2024-10-23 15:29:44 +0200

    fix

--- risultati.php
+++ risultati.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Risultati</title>
 </head>
 <body>
+
     <?php
 
     $nome = $_GET["nome"];
@@ -14,13 +15,20 @@
     $orario = $_GET["orario"];
     $mezzo = $_GET["mezzo"];
 
+    $corsi = array();
+
+    if(isset($_GET["corso1"])){
+        array_push($corsi, $_GET["corso1"]);
+    }
+    if(isset($_GET["corso2"])){
+        array_push($corsi, $_GET["corso2"]);
+    }
+    if(isset($_GET["corso3"])){
+        array_push($corsi, $_GET["corso3"]);
+    }
 
 
-
-echo"
-
-    <table>
-    
+    echo "<table style='border: 1px solid black; width: 100%; text-align: center'>
     <tr>
     <th>Nome</th>
     <th>Cognome</th>
@@ -28,25 +36,34 @@ echo"
     <th>Orario di Arrivo a Casa</th>
     <th>Mezzo di Trasporto</th>
     <th>Corsi</th>
-    </tr>
+    </tr>";
 
+    echo "
+    <tr>
+    <td>$nome</td>
+    <td>$cognome</td>
+    <td>$data</td>
+    <td>$orario</td>
+    <td>$mezzo</td>
+    <td>";
 
+    if(count($corsi) == 0){
+        echo "Nessuna attività";
+    }else{
+        foreach($corsi as $corso){
+            echo $corso . "<br>";
+        }
+    }
     
+    echo "
+    </td>
 
+    </tr>";
 
+    echo "</table>";
 
+    ?>
 
-
-
-
-
-    </table>
-
-
-    "
-    
-
-
-?>
-</body>
+    </body>
 </html>
+
